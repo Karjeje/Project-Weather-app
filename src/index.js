e2b945aa8878e54fb631e3c4702479fbd9012414
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-11-22 23:28:14 +0100

    Add default location, local storage to save data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log("Template");
 
 async function getWeather() {
   const locationInput = document.getElementById("location");
-  const location = locationInput.value;
+  const location = locationInput.value || "ljubljana";
 
   try {
     const response = await fetch(
@@ -17,17 +17,31 @@ async function getWeather() {
 
     const data = await response.json();
 
-    console.log(data);
-    console.log(processData(data));
+    const processed = processData(data);
+
+    localStorage.setItem("processedData", JSON.stringify(processed));
+    localStorage.setItem("unprocessedData", JSON.stringify(data));
+
+    console.log("Fetched and stored:", data);
+    console.log("Fetched and stored:", processed);
   } catch (err) {
     console.error("Something went wrong:", err);
   }
 }
 
+const savedUnproccesed = localStorage.getItem("unprocessedData");
+const savedProcessed = localStorage.getItem("weatherData");
+if (savedProcessed && savedUnproccesed) {
+  const unprocessedData = JSON.parse(savedUnproccesed);
+  const processedData = JSON.parse(savedProcessed);
+  console.log("Loaded from storage:", unprocessedData);
+  console.log("Loaded from storage:", processedData);
+}
+
 function processData(data) {
   return {
     location: data.address,
-    date: data.days[0].datetime,
+    dates: data.days[0].datetime,
     description: data.currentConditions.conditions,
     temperature: data.currentConditions.temp,
     feelslike: data.currentConditions.feelslike,
