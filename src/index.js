f1ead15b1b0a4872ac2370a61a27b62a1a5f1ed6
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-11-22 02:49:42 +0100

    Write function for fetching weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
 console.log("Template");
+
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=3Q2HL7K9M833AEEYCPBTU7NPX`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error("fukem");
+  }
+}
+
+getWeather("london");
